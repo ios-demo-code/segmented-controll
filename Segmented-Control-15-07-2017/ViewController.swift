1f0af65eddb5a8bed29bff73f6ed66a32c506e96
SoengSaravit <soengsaravit@gmail.com>
2017-07-15 10:22:46 +0700

    Initial Commit

---
+++ Segmented-Control-15-07-2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segmented-Control-15-07-2017
+//
+//  Created by Soeng Saravit on 7/15/17.
+//  Copyright © 2017 Soeng Saravit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
